c64700e82e475dd6949a3291f22c87ad13349617
Luke Argosino <luke.argosino@gmail.com>
2021-08-16 21:20:08 -0700

    Finished gathering JSON data from database

--- src/pages/Decks.js
+++ src/pages/Decks.js
@@ -2,20 +2,30 @@ import '../App.css'
 import { Button, Tooltip } from "@material-ui/core";
 import { BiAddToQueue } from "react-icons/bi";
 import { useHistory } from "react-router-dom";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Decks = (props) => {
-    const history = useHistory();    
-    const [data, setData] = useState([]);
-    if (!props.user) {
-        history.push('/');
-    } else {
-        props.firebase.database().ref('users/' + props.user.uid + "/").get().then((objSnapshot) => {
-            console.log(objSnapshot.val());
-        }).catch((error) => {
-            console.log(error);
-        });
-    }
+    const history = useHistory();
+    let deckNames = new Array();
+    let decks = new Array();
+    useEffect(() => {
+        if (!props.user) {
+            history.push('/');
+        } else {
+            props.firebase.database().ref('users/' + props.user.uid + "/").get().then((objSnapshot) => {
+                let value = objSnapshot.val();
+                deckNames = Object.keys(value);
+                console.log(deckNames);
+                objSnapshot.forEach(function (childSnapshot) {
+                    decks.push(childSnapshot.val());
+                });
+                // console.log(decks[0][0].backCard);
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
+    }, []);   
+
 
     return (
         <div>
